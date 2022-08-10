82db58293847ac0d7265c0ddfce7fb2f63eb7147
Lucas Silva Esmeraldino <lucas.ssilvaesmeraldino@gmail.com>
2022-08-10 08:36:55 -0300

    Commit Inicial

---
+++ src/main/java/br/com/wmw/vendafacil_frontend/VendaFacil.java
@@ -0,0 +1,16 @@
+package br.com.wmw.vendafacil_frontend;
+
+import totalcross.sys.Settings;
+import totalcross.ui.MainWindow;
+
+public class VendaFacil extends MainWindow {
+
+	public VendaFacil() {
+		setUIStyle(Settings.MATERIAL_UI);
+	}
+
+	@Override
+	public void initUI() {
+
+	}
+}
---
+++ src/main/java/br/com/wmw/vendafacil_frontend/VendaFacilApplication.java
@@ -0,0 +1,10 @@
+package br.com.wmw.vendafacil_frontend;
+
+import totalcross.TotalCrossApplication;
+
+public class VendaFacilApplication {
+
+	public static void main(final String[] args) {
+		TotalCrossApplication.run(VendaFacil.class);
+	}
+}
